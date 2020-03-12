86297c205dfc9aa1ff84668d91300f967cbc5216
YikaiZhang <171840708@smail.nju.edu.cn>
2020-03-12 23:14:28 +0800

    Update at 2020-3-12 23:14:17

--- lab_2/myswitch.py
+++ lab_2/myswitch.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 '''
 Ethernet learning switch in Python.
 
@@ -6,11 +8,13 @@ in it, not a learning switch.  (I.e., it's currently a switch
 that doesn't learn.)
 '''
 from switchyard.lib.userlib import *
+import time
 
 def main(net):
     my_interfaces = net.interfaces() 
     mymacs = [intf.ethaddr for intf in my_interfaces]
 
+    forwTable = {} # 转发表.
     while True:
         try:
             timestamp,input_port,packet = net.recv_packet()
@@ -25,8 +29,30 @@ def main(net):
         if packet[0].dst in mymacs:
             log_info("Packet intended for me")
         else:
-            for intf in my_interfaces:
-                if input_port != intf.name:
-                    log_info("Flooding packet {} to {}".format(packet, intf.name))
-                    net.send_packet(intf.name, packet)
+            isOk = False
+            # 广播
+            if str(packet[0].dst) == "ff:ff:ff:ff:ff:ff":
+                log_info("Got the broadcast packet.")
+                for intf in my_interfaces:
+                    if input_port != intf.name:
+                        log_info("Flooding packet {} to {}".format(packet, intf.name))
+                        net.send_packet(intf.name, packet)
+                        isOk = True
+
+            # 加入转发表，顺便存一下时间
+            if packet[0].src not in forwTable.keys():
+                forwTable[packet[0].src] = [input_port, time.time()]
+
+            if isOk == False:
+                # 看看转发表如果有直接发送
+                if packet[0].dst in forwTable.keys():
+                    net.send_packet(forwTable[packet[0].dst][0], packet)
+                    isOk = True
+            
+            # if isOk == False:
+            #     for intf in my_interfaces:
+            #         if input_port != intf.name:
+            #             log_info("Flooding packet {} to {}".format(packet, intf.name))
+            #             net.send_packet(intf.name, packet)
+
     net.shutdown()
