6150b01d5bb5af4ce661132243188ee66f1f1f05
YikaiZhang <171840708@smail.nju.edu.cn>
2020-03-13 20:30:31 +0800

    Update at 2020-3-13 20:30:01 Task 1 to 4 are completed

--- lab_2/myswitch_lru.py
+++ lab_2/myswitch_lru.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 '''
 Ethernet learning switch in Python.
 
@@ -6,25 +8,65 @@ in it, not a learning switch.  (I.e., it's currently a switch
 that doesn't learn.)
 '''
 from switchyard.lib.userlib import *
+import time
+from collections import deque
+
+# LRU: fowarding table 数据结构由 dictionary 换成 deque
 
 def main(net):
     my_interfaces = net.interfaces() 
     mymacs = [intf.ethaddr for intf in my_interfaces]
 
+    # log_info("My Interfaces: {}".format([intf.name for intf in my_interfaces]))
+
+    forwTable = deque(maxlen=5) # 转发表.
     while True:
         try:
             timestamp,input_port,packet = net.recv_packet()
         except NoPackets:
+            # log_info("Hit except NoPackets.")
             continue
         except Shutdown:
+            # log_info("Hit except Shutdown.")
             return
 
-        log_debug ("In {} received packet {} on {}".format(net.name, packet, input_port))
+        # log_info("In ({}) received packet ({}) on ({})".format(net.name, packet, input_port))
+        
+        # Learning step: Update forwarding table:
+        # 注意自学习的时候不更新表项。
+        # 针对src。
+        isFind = False
+
+        for idx, [val, port] in enumerate(forwTable):
+            if val == packet[0].src and port == input_port:
+                # 表中有，不用加入
+                isFind = True
+                break
+        if isFind == False:
+            # print("ADD: ", [packet[0].src, input_port])
+            forwTable.append([packet[0].src, input_port])
+
         if packet[0].dst in mymacs:
-            log_debug ("Packet intended for me")
+            # log_info("Packet intended for me")
+            pass
         else:
-            for intf in my_interfaces:
-                if input_port != intf.name:
-                    log_debug ("Flooding packet {} to {}".format(packet, intf.name))
-                    net.send_packet(intf.name, packet)
+            # 针对src，需要LRU更新：
+            dstPort = None
+            for val, port in list(forwTable):
+                if val == packet[0].dst:
+                    # print("Hit: ", [val, port])
+                    forwTable.remove([val, port])
+                    forwTable.append([val, port])
+                    dstPort = port
+                    break
+            if dstPort != None:
+                # log_info("(self-learning) Sending packet ({}) to ({})\n".format(packet, dstPort))
+                net.send_packet(dstPort, packet)
+            else:
+                for intf in my_interfaces:
+                    if input_port != intf.name:
+                        # log_info("(broadcast) Flooding packet ({}) to ({})\n".format(packet, intf.name))
+                        net.send_packet(intf.name, packet)                
+
+
     net.shutdown()
