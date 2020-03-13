5f6882c775db139620b448463a9e4168bf3c9586
YikaiZhang <171840708@smail.nju.edu.cn>
2020-03-13 12:20:43 +0800

    Update at 2020-3-13 12:20:32

--- lab_2/myswitch.py
+++ lab_2/myswitch.py
@@ -14,6 +14,8 @@ def main(net):
     my_interfaces = net.interfaces() 
     mymacs = [intf.ethaddr for intf in my_interfaces]
 
+    log_info("My Interfaces: {}".format([intf.name for intf in my_interfaces]))
+
     forwTable = {} # 转发表.
     while True:
         try:
@@ -26,25 +28,25 @@ def main(net):
             return
 
         log_info("In ({}) received packet ({}) on ({})".format(net.name, packet, input_port))
+        
+        # 加入转发表，self-learning
+        if packet[0].src not in forwTable.keys():
+            forwTable[packet[0].src] = input_port
+
         if packet[0].dst in mymacs:
             log_info("Packet intended for me")
         else:
             # 广播
-            if str(packet[0].dst) == "ff:ff:ff:ff:ff:ff" or packet[0].src not in forwTable.keys():
-                log_info("Got the broadcast packet.")
+            if packet[0].dst not in forwTable.keys():
                 for intf in my_interfaces:
                     if input_port != intf.name:
-                        log_info("Flooding packet ({}) to ({})".format(packet, intf.name))
+                        log_info("(broadcast) Flooding packet ({}) to ({})\n".format(packet, intf.name))
                         net.send_packet(intf.name, packet)
 
             else:
-                log_info("Got has been learned packet")
                 # 在转发表中，直接发送
-                if packet[0].dst in forwTable.keys():
-                    net.send_packet(forwTable[packet[0].dst], packet)
-            # 加入转发表，顺便存一下时间，self-learning
-            if packet[0].src not in forwTable.keys():
-                forwTable[packet[0].src] = input_port
+                log_info("(self-learning) Sending packet ({}) to ({})\n".format(packet, forwTable[packet[0].dst]))
+                net.send_packet(forwTable[packet[0].dst], packet)
 
 
     net.shutdown()
--- lab_2/myswitch_to.py
+++ lab_2/myswitch_to.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 '''
 Ethernet learning switch in Python.
 
@@ -6,25 +8,45 @@ in it, not a learning switch.  (I.e., it's currently a switch
 that doesn't learn.)
 '''
 from switchyard.lib.userlib import *
+import time
 
 def main(net):
     my_interfaces = net.interfaces() 
     mymacs = [intf.ethaddr for intf in my_interfaces]
 
+    log_info("My Interfaces: {}".format([intf.name for intf in my_interfaces]))
+
+    forwTable = {} # 转发表.
     while True:
         try:
             timestamp,input_port,packet = net.recv_packet()
         except NoPackets:
+            log_info("Hit except NoPackets.")
             continue
         except Shutdown:
+            log_info("Hit except Shutdown.")
             return
 
-        log_debug ("In {} received packet {} on {}".format(net.name, packet, input_port))
+        log_info("In ({}) received packet ({}) on ({})".format(net.name, packet, input_port))
+        
+        # 加入转发表，self-learning
+        if packet[0].src not in forwTable.keys():
+            forwTable[packet[0].src] = input_port
+
         if packet[0].dst in mymacs:
-            log_debug ("Packet intended for me")
+            log_info("Packet intended for me")
         else:
-            for intf in my_interfaces:
-                if input_port != intf.name:
-                    log_debug ("Flooding packet {} to {}".format(packet, intf.name))
-                    net.send_packet(intf.name, packet)
+            # 广播
+            if packet[0].dst not in forwTable.keys():
+                for intf in my_interfaces:
+                    if input_port != intf.name:
+                        log_info("(broadcast) Flooding packet ({}) to ({})\n".format(packet, intf.name))
+                        net.send_packet(intf.name, packet)
+
+            else:
+                # 在转发表中，直接发送
+                log_info("(self-learning) Sending packet ({}) to ({})\n".format(packet, forwTable[packet[0].dst]))
+                net.send_packet(forwTable[packet[0].dst], packet)
+
+
     net.shutdown()
